804858a725b47998c1674716cda24c498f0a2815
Michael Turner <mwtvy4@mst.edu>
2019-05-05 14:07:35 -0700

    collected head and shoulders data

--- Pandas explore.py
+++ Pandas explore.py
@@ -6,9 +6,9 @@ import numpy as np
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 ts = TimeSeries(key='DONT1C40C86U80WO',output_format='pandas', indexing_type='date')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='compact')
-print(data["4. close"])
-
+#data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='compact')
+#print(data["4. close"])
+#data.plot()
 #data["4. close"].plot()
 #plt.show()
 #ti = TechIndicators(key='DONT1C40C86U80WO', output_format='pandas')
@@ -22,3 +22,10 @@ print(data["4. close"])
 #df=pd.DataFrame(data)
 #print(df.iloc[2]['Real Upper Band'])
 
+##get test data
+HaS = []
+yahooCharts = ["3436.T.csv","CVX (1).csv","ITX.MC.csv","XLF.csv","GPRO.csv"]
+for chartname in yahooCharts:
+    HaS.append(pd.read_csv(chartname))
+HaS[1]['Open'].plot()
+plt.show()
