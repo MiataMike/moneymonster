74334396965acd4d52d2269b5f66a962f60e7bab
Michael Turner <mwtvy4@mst.edu>
2019-05-30 21:24:32 -0700

    abstracted classifier limits, added timer

--- dataClassifierAlpha.py
+++ dataClassifierAlpha.py
@@ -1,23 +1,27 @@
 import csv
 import math
+import time
 
+start = time.time()
 masterDataFieldnames = ['N_open0', 'N_open1', 'N_open2', 'N_open3', 'N_high0', 'N_high1', 'N_high2', 'N_high3',
              'N_low0', 'N_low1', 'N_low2', 'N_low3', 'N_close0', 'N_close1', 'N_close2', 'N_close3',
              'N_vol0', 'N_vol1', 'N_vol2', 'N_vol3', 'N_rsi', 'N_adx']
 with open('masterData.csv') as csvfile:
     reader = csv.DictReader(csvfile,fieldnames=masterDataFieldnames, quoting=csv.QUOTE_NONNUMERIC)
     for row in reader:
+        high = .05
+        medium = .025
         outputList = []
         # get tag
-        if (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= .05):
+        if (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= high):
             tag = 0
-        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= .025) & (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) < .05):
+        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= medium) & (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) < high):
             tag = 1
-        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= .0) & (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) < .025):
+        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= .0) & (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) < medium):
             tag = 2
-        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= -.025) & (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) < .05):
+        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= -medium) & (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) < .0):
             tag = 3
-        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= -.05):
+        elif (math.atanh(row['N_close0']) - math.atanh(row['N_close1']) >= -high):
             tag = 4
         else:
             tag = 5
@@ -28,3 +32,5 @@ with open('masterData.csv') as csvfile:
             writer = csv.writer(alphafile)
             writer.writerow(outputList)
 
+end = time.time()
+print("{} seconds".format(end-start))
